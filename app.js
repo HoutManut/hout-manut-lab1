97f561169140c71ce18dfb91550db4a7e06a0e67
Hout-Manut <huotmanut00@gmail.com>
2025-11-12 10:11:33 +0700

    Update greeting message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Yello, GitHub Actions!');
+    res.send('Yello, CI/CD!');
 });
 
 module.exports = app;
